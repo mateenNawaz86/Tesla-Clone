b0b0b86bc039f29c653997e1b0fe1d2c4b543084
Mateen Mirani <mateenmirani125@gmail.com>
2022-04-03 02:23:42 +0500

    customize the home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+const Home = () => {
+  return (
+    <>
+      <Container>
+        <Section />
+      </Container>
+    </>
+  );
+};
+
+export default Home;
+
+// Container styled-component
+const Container = styled.div``;
---
+++ src/components/Section.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = () => {
+  return (
+    <>
+      <Wrap>
+        <ItemText>
+          <h1>Model S</h1>
+          <p>
+            Order Online for <span>Touchless Delivery</span>
+          </p>
+        </ItemText>
+
+        <Buttons>
+          <BtnGroup>
+            <LeftBtn>Custom Order</LeftBtn>
+            <RightBtn>Existing Inventory</RightBtn>
+          </BtnGroup>
+
+          <DownArrow src="/images/down-arrow.svg" />
+        </Buttons>
+      </Wrap>
+    </>
+  );
+};
+
+export default Section;
+
+// Wrap styled-component
+const Wrap = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url("/images/model-s.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+// ItemText styled-component
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+// Buttons styled-component
+const Buttons = styled.div`
+  text-align: center;
+`;
+
+// BtnGroup styled-component
+const BtnGroup = styled.div`
+  display: flex;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+// LeftBtn styled-component
+const LeftBtn = styled.div`
+  height: 40px;
+  width: 256px;
+  border-radius: 100px;
+  color: #fff;
+  background-color: #171a20cc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 500;
+  margin: 10px;
+  cursor: pointer;
+  letter-spacing: 0.5px;
+`;
+
+// LeftBtn styled-component
+const RightBtn = styled(LeftBtn)`
+  background-color: #f4f4f4a6;
+  color: #393c41;
+`;
+
+// DownArrow styled-component
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+`;
